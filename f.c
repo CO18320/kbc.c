ccc52e1aab4e18ea5ea25d8a8820e20062b8486a
CO18320 <42859208+CO18320@users.noreply.github.com>
2018-09-02 09:02:25 -0400

    Create f.c

---
+++ f.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+
+void main()
+{
+   int q,e,r,y,u,t,a,s,d,f,g,h,j,k;
+   printf("welcome to KBC\nthe 1st question is:\n");
+   sleep(3);
+   printf("What is the current population of india?\n1. 150 crores  2. 127 crores  3. 100 crores  4. 132 crores\n");
+   
+printf("ENTER 1 2 3 or 4 according to your answer :");  
+scanf("%d",&q);
+if(q==4)
+{
+    printf("congratulations your answer is correct you have won Rs.5000\n");
+sleep(4);
+    printf("the 2nd question is:\nHow many players of kho-kho can play on field during a match?\n1. 10  2. 9  3. 8  4. 12 \nENTER 1 2 3 or 4 according to your answer : ");
+    scanf("%d",&e);
+    if(e==2)
+    {
+      printf("congratulations your answer is correct you have won Rs.10000\n");
+sleep(4);
+      printf("the 3rd question is:\nWhen did demonetisation happened?\n1. 2015 2. 2017  3. 2014  4. 2016 \nENTER 1 2 3 or 4 according to your answer :");
+        scanf("%d",&r);
+       if(r==4)
+       {
+         printf("congratulations your answer is correct you have won Rs.25000\n");
+sleep(4);
+         printf("the 4rt question is:\nIn which direction do muslim pray? ?\n1. EAST 2. NORTH  3. SOUTH  4. WEST \nENTER 1 2 3 or 4 according to your answer :");
+           scanf("%d",&y);
+            if(y==1)
+            {
+             printf("congratulations your answer is correct you have won Rs.50000\n");
+sleep(4);
+             printf("the 5th question is:\n Which is not a place in north america? ?\n1. U.S.A 2. Canada  3. Mexico  4. Brazil \nENTER 1 2 3 or 4 according to your answer :");
+              scanf("%d",&t);
+                  if(t==4)
+                  {
+                   printf("congratulations your answer is correct you have won Rs.125000\n");
+sleep(4);    
+               printf("the 6th question is:\n When was ccet degree wing established ?\n1. 2000  2. 2001  3. 2002  4. 2003 \nENTER 1 2 3 or 4 according to your answer :");  
+                    scanf("%d",&a);
+                       if(a==3)
+                        {
+                         printf("congratulations your answer is correct you have won Rs.125000\n");
+sleep(4);
+                         printf("the 7th question is:\n Which of the following is not a computing language ?\n1. C  2. C quora  3. C sharp  4. C# \nENTER 1 2 3 or 4 according to your answer :"); 
+                          scanf("%d",&d);
+                            if(d==2)
+                             {
+                             printf("congratulations your answer is correct you have won Rs.250000\n");
+sleep(4);
+                             printf("the 8th question is:\n Which sector is not present in chandigarh ?\n1. 23  2. 49  3. 13  4. 5 \nENTER 1 2 3 or 4 according to your answer :");
+                               scanf("%d",&s);
+                                 if(s==3)
+                                   {
+                                     printf("congratulations your answer is correct you have won Rs.600000\n");
+sleep(4);
+                                     printf("the 9th question is:\n Who is the current CEO of apple? ?\n1. Steve Jobs  2. Tim Cook  3. Steve Woznaik  4. Al Khalid \nENTER 1 2 3 or 4 according to your answer :");
+                                      scanf("%d",&f);
+                                    if(f==2)
+                                    {
+                                    printf("congratulations your answer is correct you have won Rs.1250000\n");
+sleep(4);
+                                    printf("the 10th question is:\n Which is the first trillion dollar company? ?\n1. Amazon  2. Apple  3. Quilla  4. Pepsico \nENTER 1 2 3 or 4 according to your answer :");
+                                     scanf("%d",&h);
+                                       if(h==2)
+                                       {
+                                     printf("congratulations your answer is correct you have won Rs.2500000");
+sleep(4);
+                                      printf("the 11th question is:\n Which is India's first satellite ?\n1. Arjuna  2. Aryabhatta  3. Akashganga  4. Kuldeep 1 \nENTER 1 2 3 or 4 according to your answer :");
+                                       scanf("%d",&j);
+                                        if(j==2)
+                                         {
+                                          printf("congratulations your answer is correct you have won Rs.5000000\n");
+sleep(2);          
+                                         printf("congratulations for coming this far we hope that you also answer the last question correctly\n");
+sleep(2);
+                                         printf("the 12th and the last question is: What is the net worth of Jeff Bezos?\n 1. 151 billion dollars 2. 161 billion dollars 3. 145 billion dollars 4. none of the above"); 
+                                        scanf("%d",&k);
+                                          if(k==2)
+                                           {
+                                           printf("congratulations your answer is correct you have won Rs.10000000");
+                                           }
+                                            else {
+  printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.2500000");                   }   
+                                                 
+                                         }
+                                         else
+                                         { printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.1250000");                   }   
+                                       }
+                                      else 
+                                       {
+                                       printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.600000");   
+                                       }
+                                    }  
+                                    else 
+                                   {
+                                  printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.250000");
+                                 }
+                      
+                                    }
+                                 else 
+                                 {
+                         printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.250000");
+                                 }
+
+                              }  
+                              else
+                                {
+                         printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.50000");
+                                }
+                             
+                            }
+                        
+                       }
+                        else 
+                         {
+                         printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.50000");
+                         }
+                      
+              }
+                 else
+                {
+                 printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.10000");
+                }
+
+        }
+        else
+        {
+         printf("sorry your answer is not correct better luck next time, but dont worry you are taking a good amount of Rs.10000");
+        }
+    
+
+
+     }
+    else
+    {
+    printf("sorry your answer is not correct better luck next time.");
+    }
+    
+}
+else
+{
+    printf("sorry your answer is not correct better luck next time.");
+}
+}
